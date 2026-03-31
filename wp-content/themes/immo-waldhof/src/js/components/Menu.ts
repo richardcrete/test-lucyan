548569e003f089db2a64ebf45117f2357cc4394c
richardcrete <crete.richard.ritchi@gmail.com>
2026-03-31 22:57:25 +0200

    Fix desktop Menu after scroll

--- wp-content/themes/immo-waldhof/src/js/components/Menu.ts
+++ wp-content/themes/immo-waldhof/src/js/components/Menu.ts
@@ -5,29 +5,37 @@ export default class MenuMobile {
     public static searchForm: HTMLDivElement | null = document.querySelector(".SearchForm");
 
     public static init(): void {
-        this.checkScroll();
+        this.afterScroll();
 
         if (this.searchForm) {
-            window.addEventListener("scroll", () => {
-                this.checkScroll();
+            window.addEventListener("scroll", (): void => {
+                this.afterScroll();
             });
         }
 
-        this.menuItems.forEach((menuItem: HTMLLIElement) => {
-            menuItem.addEventListener("mouseenter", () => {
-                this.addClasses();
+        this.menuItems.forEach((menuItem: HTMLLIElement): void => {
+            menuItem.addEventListener("mouseenter", (): void => {
+                if (!this.checkScroll()) {
+                    this.addClasses();
+                }
             });
         });
 
-        this.menuItems.forEach((menuItem: HTMLLIElement) => {
-            menuItem.addEventListener("mouseleave", () => {
-                this.removeClasses();
+        this.menuItems.forEach((menuItem: HTMLLIElement): void => {
+            menuItem.addEventListener("mouseleave", (): void => {
+                if (!this.checkScroll()) {
+                    this.removeClasses();
+                }
             });
         });
     }
 
-    public static checkScroll(): void {
-        if (this.searchForm && this.searchForm.getBoundingClientRect().top < 100) {
+    public static checkScroll(): boolean | null {
+        return (this.searchForm && this.searchForm.getBoundingClientRect().top < 100);
+    }
+
+    public static afterScroll(): void {
+        if (this.checkScroll()) {
             this.addClasses();
         } else {
             this.removeClasses();
