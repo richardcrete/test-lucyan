9734c093064b3818199291778073b305191fa231
richardcrete <crete.richard.ritchi@gmail.com>
2026-04-01 20:06:13 +0200

    Add favicons to admin

--- wp-content/themes/immo-waldhof/functions.php
+++ wp-content/themes/immo-waldhof/functions.php
@@ -15,3 +15,12 @@ function allow_svg_uploads($mimes)
 }
 
 add_filter('upload_mimes', 'allow_svg_uploads');
+
+function add_favicon_to_admin()
+{
+    echo '<link rel="icon" type="image/png" href="' . get_template_directory_uri() . '/src/assets/favicon/favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/svg+xml" href="' . get_template_directory_uri() . 'src/assets/favicon/favicon.svg"/>
+    <link rel="shortcut icon" href="' . get_template_directory_uri() . 'src/assets/favicon/favicon.ico" />';
+}
+
+add_action('admin_head', 'add_favicon_to_admin'); //admin end
