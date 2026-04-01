4f64953a3eb428459d4576c0d0cba55b5e167859
richardcrete <crete.richard.ritchi@gmail.com>
2026-04-01 20:05:02 +0200

    Fix favicons

--- wp-content/themes/immo-waldhof/header.php
+++ wp-content/themes/immo-waldhof/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <title><?= get_the_title() . ' - ' . get_bloginfo('name') ?></title><link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <title><?= get_the_title() . ' - ' . get_bloginfo('name') ?></title>
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri() ?>/src/assets/favicon/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="<?= get_template_directory_uri() ?>/src/assets/favicon/favicon.svg"/>
     <link rel="shortcut icon" href="<?= get_template_directory_uri() ?>/src/assets/favicon/favicon.ico" />
 </head>
