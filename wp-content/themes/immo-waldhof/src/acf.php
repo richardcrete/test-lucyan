a99d1b127eb415f4cc7607b41d2250977116cba6
richardcrete <crete.richard.ritchi@gmail.com>
2026-04-01 11:08:40 +0200

    Dynamic BigCTA ACF block

--- wp-content/themes/immo-waldhof/src/acf.php
+++ wp-content/themes/immo-waldhof/src/acf.php
@@ -4,6 +4,19 @@ add_action('acf/init', 'register_acf_blocks');
 function register_acf_blocks(): void
 {
     acf_register_block_type([
+        'name' => 'big-cta',
+        'title' => 'Big CTA',
+        'description' => 'Gros CTA',
+        'icon' => array(
+            'background' => '#fff',
+            'foreground' => '#000',
+            'src' => 'megaphone',
+        ),
+        'render_template' => get_template_directory() . '/template-parts/blocks/big-cta.php',
+        'enqueue_script' => get_template_directory_uri() . '/dist/app.js',
+    ]);
+
+    acf_register_block_type([
         'name' => 'reviews',
         'title' => 'Avis',
         'description' => 'Avis Google',
---
+++ wp-content/themes/immo-waldhof/template-parts/blocks/big-cta.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Big CTA Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+?>
+
+<div class="BigCTA isComponent<?= get_field('background') ? ' isLight' : '' ?><?= get_field('position') ? ' isHigher' : '' ?>">
+    <div class="container">
+        <div class="BigCTA-content">
+            <div class="BigCTA-title">
+                <h3 class="h3"><?= get_field('sub_title') ?></h3>
+                <h2 class="h1"><?= get_field('title') ?></h2>
+            </div>
+            <a href="<?= get_field('link')['url'] ?>" target="<?= get_field('link')['target'] ?>" class="Button isTertiary isMedium isBorderRadius">
+                <?= get_field('link')['title'] ?>
+            </a>
+        </div>
+    </div>
+</div>
