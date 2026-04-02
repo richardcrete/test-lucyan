97ac994dbc8b3ceb39a2065e6ab8866ef0342a8c
richardcrete <crete.richard.ritchi@gmail.com>
2026-04-02 05:24:52 +0200

    Fix favicons

--- wp-content/themes/immo-waldhof/functions.php
+++ wp-content/themes/immo-waldhof/functions.php
@@ -19,8 +19,8 @@ add_filter('upload_mimes', 'allow_svg_uploads');
 function add_favicon_to_admin()
 {
     echo '<link rel="icon" type="image/png" href="' . get_template_directory_uri() . '/src/assets/favicon/favicon-96x96.png" sizes="96x96"/>
-    <link rel="icon" type="image/svg+xml" href="' . get_template_directory_uri() . 'src/assets/favicon/favicon.svg"/>
-    <link rel="shortcut icon" href="' . get_template_directory_uri() . 'src/assets/favicon/favicon.ico"/>';
+    <link rel="icon" type="image/svg+xml" href="' . get_template_directory_uri() . '/src/assets/favicon/favicon.svg"/>
+    <link rel="shortcut icon" href="' . get_template_directory_uri() . '/src/assets/favicon/favicon.ico"/>';
 }
 
 add_action('admin_head', 'add_favicon_to_admin');
